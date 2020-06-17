3f3e320fc114df2a390e1c3fbc0269e7c383269e
Juanjo <jninahualpa@gmail.com>
2020-06-17 17:07:20 -0500

    Updates

---
+++ precache-manifest.c66eaece64cb187724179433829ffec3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247cec5e59e13277a45c125c0eeeee90",
+    "url": "/ReactDeploy/index.html"
+  },
+  {
+    "revision": "900b3c6e9bf2bde1f0a1",
+    "url": "/ReactDeploy/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "a516b4d5c7b02f60d08d",
+    "url": "/ReactDeploy/static/css/main.89c9f429.chunk.css"
+  },
+  {
+    "revision": "900b3c6e9bf2bde1f0a1",
+    "url": "/ReactDeploy/static/js/2.50f8cc72.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/ReactDeploy/static/js/2.50f8cc72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a516b4d5c7b02f60d08d",
+    "url": "/ReactDeploy/static/js/main.419ebf7d.chunk.js"
+  },
+  {
+    "revision": "3bf5c394a9f7b39c821d",
+    "url": "/ReactDeploy/static/js/runtime-main.196404e6.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/ReactDeploy/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/ReactDeploy/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/ReactDeploy/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/ReactDeploy/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/ReactDeploy/static/media/stars.1d17360a.svg"
+  }
+]);
